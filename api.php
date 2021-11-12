103aa6c1822afc089f09a7ca1d3d4736cc6b60c7
tuliojunior1 <rafael.tulio@accesscontact.com.br>
2021-11-12 15:12:54 -0300

    teste

--- api.php
+++ api.php
@@ -16,6 +16,7 @@
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_TIMEOUT , 260 );
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
