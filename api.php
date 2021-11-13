67ddd5bf45bbbf1b444033a20790f5c6cbf3c425
tuliojunior1 <rafael.tulio@accesscontact.com.br>
2021-11-13 15:14:04 -0300

    Update api.php

--- api.php
+++ api.php
@@ -7,13 +7,14 @@
         $url = 'http://homolog.saneago.com.br/prt/';
         // $url = '172.16.1.171/prt/';
         // $token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjaGF2ZXNEZUFjZXNzbyI6WyJFQUNTMDAxfENBSUUiLCJCQVBTMDAxfEMqKioiLCJGR0NWMDA3fEMqKioiLCJFQ09TMDE5fEMqKioiLCJFQ09TMDA2fEMqKioiLCJFQ09TMDAxfENBSUUiLCJFQ09TMDAyfEMqKioiLCJFR1dTMDAxfEMqKioiLCJFQ09TMDAzfENBSSoiLCJFQ09TMDA0fEMqKioiLCJFQ09TMDIxfEMqKioiLCJNUFNTMDAxfEMqKioiLCJNUFNTMDAyfEMqKioiLCJFQ09TMDE2fEMqKioiLCJFQ09TMDA1fEMqKioiLCJFQ09TMDE0fEMqKioiLCJFQ09TMDE3fEMqKioiLCJFQ09TMDA4fEMqKioiLCJHUE1TMDAxfEMqKioiLCJFQ09TMDA3fENBSSoiLCJFQ09TMDIwfEMqKioiLCJFQ09TMDEwfEMqKioiLCJFQ09TMDExfEMqKioiLCJNUFNTMDAzfEMqKioiLCJFQ09TMDEyfEMqKioiLCJFQ09TMDEzfEMqKioiLCJFQUNTMDAyfCoqSSoiLCJNU1NTMDAxfEMqKioiLCJNU1NTMDAyfEMqKioiLCJPU0xTMDAxfEMqKioiLCJFQ09TMDE1fEMqKioiLCJFQ09TMDA5fEMqKioiLCJFQ09TMDE4fEMqKioiLCJNUFNTMDA0fEMqKioiXSwiZXhwIjoxNjM2ODI3NTI2LCJ1c2VybmFtZSI6Ik1TSTAwMTEifQ.VEXzze1bJNSgjKbThsqCv0AjPgbMmFYWe4JnlJUtdyc";
-        $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjaGF2ZXNEZUFjZXNzbyI6WyJCQVBTMDAxfENBSUUiLCJFQUNTMDAxfENBSUUiLCJFQUNTMDAyfENBSUUiLCJFQ09TMDAxfENBSUUiLCJFQ09TMDAyfENBSUUiLCJFQ09TMDAzfENBSUUiLCJFQ09TMDA0fENBSUUiLCJFQ09TMDA1fENBSUUiLCJFQ09TMDA2fENBSUUiLCJFQ09TMDA3fENBSUUiLCJFQ09TMDA4fENBSUUiLCJFQ09TMDA5fENBSUUiLCJFQ09TMDEwfENBSUUiLCJFQ09TMDExfENBSUUiLCJFQ09TMDEyfENBSUUiLCJFQ09TMDEzfENBSUUiLCJFQ09TMDE0fENBSUUiLCJFQ09TMDE1fENBSUUiLCJFQ09TMDE2fENBSUUiLCJFQ09TMDE3fENBSUUiLCJFQ09TMDE4fENBSUUiLCJFQ09TMDE5fENBSUUiLCJFQ09TMDIwfENBSUUiLCJFQ09TMDIxfEMqKioiLCJFR1dTMDAxfENBSUUiLCJHUE1TMDAxfENBSUUiLCJNUFNTMDAxfENBSUUiLCJNUFNTMDAyfENBSUUiLCJNUFNTMDAzfENBSUUiLCJNUFNTMDA0fENBSUUiLCJNU1NTMDAxfENBSUUiLCJNU1NTMDAyfENBSUUiLCJPU0xTMDAxfENBSUUiXSwiZXhwIjoxNjM2ODI2NjkyLCJ1c2VybmFtZSI6Ik1TSTAwMTEifQ.n2jh6OWapbXRDfQ4hc4XH05NdmB6tdPqxlJaPHnkZJE";
+        if(isset($_POST['token'])){
+            $getToken = $_POST['token'];
+        }
         $header = array();
         $header[] = 'Content-type: application/json';
         // $header[] = 'Content-type: application/x-www-form-urlencoded';
         if($params['url']!='ws/login'){
-            $header[] = 'token: '.(string)$token;
-            // $header[] = 'Authorization: '.$token;
+            $header[] = 'token: '.(string) $getToken;
         }
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -83,7 +84,7 @@
             "url" => 'ws/eac/protocolo',
             "metodo" => 'POST',
             "header" => 'token',
-            "params" => ["telefone" => "47992869383"],
+            "params" => ["telefone" => $getTelefone],
             "resposta" => "protocolo",
             "status" => 'Código de status: 201 Created'
         ],
@@ -135,6 +136,6 @@
     echo "<pre>";
     $request = $_POST['request'];
     // $get = requisitarApi($params['login']);
-    $get = requisitarApi($params[$request]);
-    var_dump($get);
+    echo $get = requisitarApi($params[$request]);
+    // var_dump($get);
   
